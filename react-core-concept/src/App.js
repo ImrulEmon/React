039bcd0fcb9a938ac8a39b3456b4b3c896504ebe
ImrulEmon <emimrulemon@gmail.com>
2022-04-01 17:47:37 +0600

    :electeron:

--- react-core-concept/src/App.js
+++ react-core-concept/src/App.js
@@ -2,40 +2,28 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  let number = 50000;
-  const singer={
-    name : 'Mr Fox',
-    job : 'musician'
-  }
-  const singerStyle ={
-    color : "#61DAFB",
-    fontStyle : "italic",
-    fontFamily:"monospace"
-  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <div style={singerStyle} className="container">
-          <p style={{color:"white"}}>I am here</p> 
-          <p>My number = {number+12}</p>
-          <p>Singer : {singer.name} | Job : {singer.job.toUpperCase()}</p>
-        </div>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Person></Person>
+      <Person></Person>
+      <Person></Person>
     </div>
   );
+}
+function Person() {
+  const personStyle ={
+    width:"50%",
+    borderRadius:"10px",
+    margin:"5% auto"
+
+  }
+  return (
+    <div style={personStyle} className='Person'>
+      <h3 style={{backgroundColor:"skyblue",borderRadius:"5px",width:'50%',margin:"2%auto"}}>Imrul Emon</h3>
+      <p style={{ fontWeight: 'bold', fontStyle: "italic", color: "orange" }}>Job : Student</p>
+    </div>
+  )
 }
 
 export default App;
