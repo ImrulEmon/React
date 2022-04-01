32ff0b32dcd9a6250785c4df712b4885f8177027
ImrulEmon <emimrulemon@gmail.com>
2022-04-01 16:57:05 +0600

    :electron::electron::electron:

--- react-core-concept/src/App.js
+++ react-core-concept/src/App.js
@@ -2,13 +2,29 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let number = 50000;
+  const singer={
+    name : 'Mr Fox',
+    job : 'musician'
+  }
+  const singerStyle ={
+    color : "#61DAFB",
+    fontStyle : "italic",
+    fontFamily:"monospace"
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <div style={singerStyle} className="container">
+          <p style={{color:"white"}}>I am here</p> 
+          <p>My number = {number+12}</p>
+          <p>Singer : {singer.name} | Job : {singer.job.toUpperCase()}</p>
+        </div>
         <a
           className="App-link"
           href="https://reactjs.org"
