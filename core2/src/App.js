ff75ffcfe28f7c943d2afa5ddd8f960aee4cdb29
ImrulEmon <emimrulemon@gmail.com>
2022-04-02 01:17:01 +0600

    :electron::four::five:✔

--- core2/src/App.js
+++ core2/src/App.js
@@ -1,14 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
     <div className="App">
       <Counter></Counter>
+      <ExternalUsers></ExternalUsers>
     </div>
   );
 }
+
+
+function User(props) {
+  return(
+    <div className='user'>
+      <h3>Name : {props.name}</h3>
+      <p>Email : {props.email}</p>
+    </div>
+  )
+}
+
 
 function Counter(props) {
  const [count,setCount] = useState(0);
@@ -22,5 +34,23 @@ const handleDecrease = () => setCount(count-1);
     </div>
   )
 }
+
+function ExternalUsers() {
+  const[users,setUsers]=useState([]);
+  useEffect(()=>{
+    fetch(`https://jsonplaceholder.typicode.com/users`)
+    .then(res=>res.json())
+    .then(data=>setUsers(data));
+  },[])
+  return(
+    <div>
+      <h3>External Users</h3>
+      {
+        users.map(user =><User name={user.name} email={user.email}></User>)
+      }
+    </div>
+  )
+}
+
 
 export default App;
